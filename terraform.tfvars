21bd36de037f14b63cebbc6b6916be90c043ab15
Pranay Agrawal <pranay3agrawal@gmail.com>
2021-07-22 20:29:05 +0000

    version 1

---
+++ terraform.tfvars
@@ -0,0 +1,9 @@
+#Varaibles of AWS
+
+aws-instance-type= "t2.micro"
+aws-key-name= "Nvirginiakey"
+aws-ami= "ami-0d5eff06f840b45e9"
+aws-availability-zone= "us-east-1a"
+public-ip= "true"
+
+
---
+++ varibales.tf
@@ -0,0 +1,28 @@
+variable "aws-instance-type" {}
+
+variable "aws-availability-zone"{}
+
+variable "aws-key-name" {}
+
+variable "aws-ami" {}
+
+variable "aws-master-tag" {
+     type= list
+     default= ["master"]
+}
+
+variable "aws-worker-tag"{
+        type=list
+        default=["worker1","worker2"]
+}
+
+variable "public-ip" {}
+
+
+variable "region" {
+  default = "us-east-1"
+}
+
+
+
+
