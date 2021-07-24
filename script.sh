5c3e2e1e4f9a7b73dcfb4a35dbaab467e597fcd1
Pranay agrawal <66831635+pranay1603@users.noreply.github.com>
2021-07-25 00:11:04 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ sudo yum install git -y
 
 
 
-mkdir admin/;sudo cp /etc/kubernetes/admin.conf admin/;cd admin/;sudo git init;sudo git add .;sudo git commit -m "version 1" .;sudo git push https://pranay1603:Pagrawal1107@github.com/pranay1603/k8s-confile.git --all
+mkdir admin/;sudo cp /etc/kubernetes/admin.conf admin/;cd admin/;sudo git init;sudo git add .;sudo git commit -m "version 1" .;sudo git push https://<username>:<password>@github.com/pranay1603/k8s-confile.git --all
 
